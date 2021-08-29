9e2b266ef878d5e3365333db3ad7b6be2d74f35a
Zelipha <zeliphawambui756@gmail.com>
2021-08-29 09:28:30 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ * Description: prints single digits of base 10 starting from 0
+ * Return: 0
+*/
+	int main(void)
+{
+
+	int s = 0;
+
+	while (s <= 9)
+		{
+	printf("%d\n", s);
+	s++;
+		}
+		return (0);
+}
