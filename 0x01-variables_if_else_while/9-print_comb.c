2980eca38172f0d398140137792575547dd4464d
Zelipha <zeliphawambui756@gmail.com>
2021-08-30 10:55:59 -0700

    Combination of single-digit numbers separated by ,

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,12 +12,12 @@ int main(void)
 
 	for (n = '0'; n <= '9'; n++)
 	{
-		putchar(n + '0');
+		putchar(n);
 
 		if (n != '9')
 		{
 			putchar(' ');
-		        putchar(',');
+			putchar(',');
 		}
 	}
 	putchar('\n');
