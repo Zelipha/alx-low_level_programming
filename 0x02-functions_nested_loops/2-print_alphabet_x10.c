6b84e643f2a52ec19b9940c9222d2c43e2cb0650
Zelipha <zeliphawambui756@gmail.com>
2021-08-31 10:26:59 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - entry point
+ * Description: program that prints alphabet in lowercase 10 times
+ *
+ * Return: 0
+*/
+void print_alphabet_x10(void)
+{
+	int alpha;
+	int i = 0;
+
+	while (i <= 9)
+	{
+		for (alpha = 'a'; a <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+
+		i++
+			}
+}
