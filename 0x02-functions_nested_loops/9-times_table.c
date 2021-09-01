eadd20519555e6be5d79ab1e8ed4cd7a178934b0
Zelipha <zeliphawambui756@gmail.com>
2021-09-01 05:08:48 -0700

    prints 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ *
+ * times_table - Prints the 9 times table, starting from 0.
+ *
+ */
+void times_table(void)
+{
+	int i, j, r, f, s;
+
+	r = 0;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			r = i * j;
+			if (j > 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (r >= 10 && r < 100)
+				{
+					f = r / 10;
+					s = r % 10;
+					_putchar('0' + f);
+					_putchar('0' + s);
+				}
+				else if (r < 10)
+				{
+					_putchar(' ');
+					_putchar('0' + r);
+				}
+			}
+
+		}
+	_putchar('\n');
+	}
+}
