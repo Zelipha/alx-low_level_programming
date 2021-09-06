1a89febf939a1d5a00834859edeed942578ab76d
Zelipha <zeliphawambui756@gmail.com>
2021-09-06 11:07:28 -0700

    Returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of a string
+ * @s: character to check
+ * Return: 0;
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0';)
+		++i;
+
+	return (1);
+}
