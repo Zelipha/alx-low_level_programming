64f2557ea2fe1abeba2937bb2550311c002128d7
Zelipha <zeliphawambui756@gmail.com>
2021-09-13 10:20:10 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+* _strchr - Entry point
+* Description - A function that locates a character in a string
+* *@s: the function accepts an input saved into s
+* *@c: the function accepts an input saved into c
+* Return Always 0
+*/
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] >= '\0')
+
+	{
+		if (s[i] == c)
+			return (&s[i]);
+		i++;
+	}
+	return (NULL);
+}
