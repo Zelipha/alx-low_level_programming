aa9e51eb7c84473fc3158b3e6f6b1704bd9d93be
Zelipha <zeliphawambui756@gmail.com>
2021-09-13 10:19:21 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* _memcpy - Entry point
+* Description - A function that copies memory area
+* *@dest: the function accepts an input saved into dest
+* *@src: the function accepts an input saved into src
+* *@n: the function accepts an input saved into n
+* Return 0;
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*(dest + i) = src[i];
+		i++;
+	}
+
+	return (dest);
+}
