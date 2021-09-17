a994e190192b52b20eeaf90cebcfdae147a1bd7a
Zelipha <zeliphawambui756@gmail.com>
2021-09-17 08:22:42 -0700

    prints name of the program

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,9 +6,8 @@
  * Return: 0
  */
 
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-      	printf("%s\n", argv[0]);
-
+	printf("%s\n", argv[0]);
 	return (0);
 }
