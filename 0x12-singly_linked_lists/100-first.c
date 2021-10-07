7abf1a2660ed760e0a027e95e75d55e586428aa7
Zelipha <zeliphawambui756@gmail.com>
2021-10-07 08:32:12 -0700

    A function that executes before the main function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * before_main - entry point
  *
  * Return: Always 0
  */
 void __attribute__ ((constructor)) before_main()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
