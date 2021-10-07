bbb37cbd8e63e7f653e8ca49f11423d51e7ea003
Zelipha <zeliphawambui756@gmail.com>
2021-10-07 08:19:58 -0700

    Add README.md file

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+void __attribute__ ((constructor)) before_main()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
