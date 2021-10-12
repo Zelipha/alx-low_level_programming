e0f988e91520c26b900d0d720ba00d0442493e91
Zelipha <zeliphawambui756@gmail.com>
2021-10-12 15:14:06 -0700

    Another solution

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,21 +1,25 @@
 #include "lists.h"
 
 /**
- * add_nodeint - adds a new node at the beginning of a list
- * Description - returns the address of the new element, or NULL if it failed
- * @head: pointer to the start of the list
- * @n: integer to be copied
- * Return: new node
+ * add_nodeint - add a new node at the beginning
+ * of a linked list
+ * @head: head of a list.
+ * @n: n element.
+ *
+ * Return: address of the new element. NUll if it failed.
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
 	new = malloc(sizeof(listint_t));
+
 	if (new == NULL)
 		return (NULL);
+
 	new->n = n;
 	new->next = *head;
 	*head = new;
-	return (new);
+
+	return (*head);
 }
