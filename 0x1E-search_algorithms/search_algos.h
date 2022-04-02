11fb5cf9a67b765fb64a61dbe8007c3f54d7c658
Zelipha <zeliphawambui756@gmail.com>
2022-04-02 11:39:08 +0300

    Update header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,7 +6,8 @@
 #include <stdlib.h>
 
 /**
-  * @n: Integer
+  * struct listint_s - singly linked list
+  * @n: Integer.
   * @index: Index of the node in the list
   * @next: Pointer to the next node
   *
